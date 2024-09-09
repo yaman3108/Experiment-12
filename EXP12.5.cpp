543722bd95959d2aca70811906c2aed6ccbc1427
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-09-09 13:43:42 +0530

    Create EXP12.5.cpp

---
+++ EXP12.5.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class student
+{
+    int rn;
+    char n[50];
+    float avg = 0;
+    public:
+    student(int,char[],float);
+
+    student(student &t)
+    {
+        rn=t.rn;
+        strcpy(n,t.n);
+        avg=t.avg;
+    }
+    void display();
+
+};
+
+ student::student(int rno,char na[],float av)
+ {
+    rn=rno;
+    strcpy(n,na);
+    avg=av;
+ }
+
+void student::display()
+ {
+    cout<<endl;
+    cout<<"Name: "<<n<<endl;
+    cout<<"Roll No: "<<rn<<endl;
+    cout<<"Average: "<<avg<<" %"<<endl;
+ }
+
+int main()
+{
+    student s1(155,"yaman",93.7);
+    s1.display();
+
+    student yaman(s1);
+    yaman.display();
+
+    return 0;
+}
